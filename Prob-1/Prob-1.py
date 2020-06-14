eb5ef9517bfabb7150c95c0bb545657923393e29
tylerjjohnson64 <t.johnson57@students.clark.edu>
2020-06-13 18:30:31 -0700

    completed

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -3,3 +3,51 @@
 #     Prob-1.py
 
 # Your code below
+class car:
+    def __init__(self,make,model,year):
+        self._make = make
+        self._model = model
+        self._year = year
+
+    def set_make(self,make):
+        self._make = make
+    def get_make(self):
+        return self._make
+    def set_model(self,model):
+        self._model = model
+    def get_model(self):
+        return self._model
+    def set_year(self,year):
+        self._year = year
+    def get_year(self):
+        return self._year
+
+    
+ 
+def Testcar():
+   carlot = []
+   carlot.append(car("toyota","tacoma","1994"))
+   carlot.append(car("ford","f150","2008"))
+   carlot.append(car("honda","accord","1990"))
+   carlot.append(car("toyota","corolla","2005"))
+   carlot.append(car("gmc","sierra","1999")) 
+   print("Make---Model---Year")
+   for vehicle in carlot:
+      
+       print(vehicle.get_make(),vehicle.get_model(),vehicle.get_year())
+       print()
+       
+   for vehicle in carlot:
+       vehicle.set_make(input("enter a make: "))
+       vehicle.set_model(input("enter a model: "))
+       vehicle.set_year(input("enter a year: "))
+    
+   for vehicle in carlot:
+       print(vehicle.get_make(),vehicle.get_model(),vehicle.get_year())
+       print()
+    
+
+Testcar()
+
+if __name__ == "__main__":
+    Testcar()
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,4 +2,53 @@
 #   Programming Assignment 11
 #     Prob-2.py
 
-# Your code below
+class Student:
+    def __init__(self,name,IDnum,major,GPA):
+        self._name = name
+        self._IDnum = IDnum
+        self._major = major
+        self._GPA = GPA
+
+    def set_name(self,name):
+        self._name = name
+    def get_name(self):
+        return self._name
+    def set_IDnum(self,IDnum):
+        self._IDnum = IDnum
+    def get_IDnum(self):
+        return self._IDnum
+    def set_major(self,major):
+        self._major = major
+    def get_major(self):
+        return self._major 
+    def set_GPA(self,GPA):
+        self._GPA = GPA
+    def get_GPA(self):
+        return self._GPA
+def studentprog ():
+    studentlist = []
+    studentlist.append(Student("Joe Bella", "9933","",""))
+    studentlist.append(Student("Tucker Blank", "3399","",""))
+    studentlist.append(Student("Gayle Ujifusa", "9875","",""))
+    studentlist.append(Student("Edna Anker", "9875","",""))
+#chose to set id number and student name as constant since these are not very often subject to change
+    studentlist[0].set_major("Programmer")
+    studentlist[0].set_GPA("2.5")
+    studentlist[1].set_major("Basket Weaving")
+    studentlist[1].set_GPA("3.9")
+    studentlist[2].set_major("Janitor")
+    studentlist[2].set_GPA("3.2")
+    studentlist[3].set_major("Zoo Keeper")
+    studentlist[3].set_GPA("1.9")
+#since these are items subject to change and fluctuate it made more sent to set these seperately 
+
+    for stu in studentlist:
+        print(stu.get_name())
+        print(stu.get_IDnum())
+        print(stu.get_major())
+        print(stu.get_GPA())
+        
+        print()
+
+studentprog()
+
